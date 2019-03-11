aabb771323ab62f91573eb952769176e5433e87d
Andrew Zhu <me@andrew-zhu.com>
2019-03-11 14:38:09 -0700

    add interactive evalutation

--- spell_evaluation.py
+++ spell_evaluation.py
@@ -1,9 +1,11 @@
 import json
+import sys
 import time
 
 import numpy as np
 import tensorflow as tf
 from fuzzywuzzy import fuzz, process
+from tabulate import tabulate
 
 from preprocess import MAGIC_1, MAGIC_2, clean, tokenize
 
@@ -37,12 +39,17 @@ def load_evaluation_queries():
     return data
 
 
-def naive_partial_match(choices, query):
+def naive_partial_match(choices, query, return_weights=False):
     """Returns the names of the top 5 results using this search algorithm."""
     full_matches = [s['name'] for s in choices if s['name'].lower() == query.lower()]
     partial_matches = [s['name'] for s in choices if
                        query.lower() in s['name'].lower() and s['name'] not in full_matches]
-    return full_matches + partial_matches
+    results = full_matches + partial_matches
+    if not return_weights:
+        return results
+    weights = [len(query) / len(r) for r in results]
+    weighted_results = sorted(list(zip(results, weights)), key=lambda e: e[1], reverse=True)
+    return weighted_results
 
 
 def naive_levenshtein_distance(choices, query):
@@ -59,7 +66,7 @@ def naive_levenshtein_distance(choices, query):
     return results
 
 
-def pure_model(choices, query, model, magic_string, model_name):
+def pure_model(choices, query, model, magic_string, model_name, return_weights=False):
     query = clean(query)
     query = tokenize(query, magic_string)
     query = np.expand_dims(query, 0)
@@ -71,11 +78,12 @@ def pure_model(choices, query, model, magic_string, model_name):
 
     indexed = list(enumerate(prediction))
     weighted = sorted(indexed, key=lambda e: e[1], reverse=True)
+    if not return_weights:
+        return [choices[r[0]]['name'] for r in weighted[:10]]
+    return [(choices[r[0]]['name'], r[1]) for r in weighted[:10]]
 
-    return [choices[r[0]]['name'] for r in weighted[:10]]
 
-
-def mixed_model(choices, query, model, magic_string, model_name):
+def mixed_model(choices, query, model, magic_string, model_name, return_weights=False):
     names = [s['name'] for s in choices]
     fuzzy_results = process.extract(query, names, scorer=fuzz.ratio)
     fuzzy_sum = max(sum(r[1] for r in fuzzy_results), 0.001)
@@ -99,11 +107,14 @@ def mixed_model(choices, query, model, magic_string, model_name):
 
     # build results list, unique
     results = []
+    weights = []
     for r in sorted_weighted:
         if r[0] not in results:
             results.append(r[0])
-
-    return results
+            weights.append(r[1])
+    if not return_weights:
+        return results
+    return list(zip(results, weights))
 
 
 def evaluate(search, query_pairs, choices, model=None, reverse_map=None, magic_string=None, model_name=None):
@@ -136,6 +147,42 @@ def evaluate(search, query_pairs, choices, model=None, reverse_map=None, magic_s
     return top_1, top_2, top_3, fail, end - start, top_10
 
 
+def interactive_search(choices, models, map_, last_model, last_model_name):
+    if not len(models):
+        print("At least 1 model must be evaluated for interactive search")
+        return
+    while True:
+        query = input("Query? ")
+        top_naive_partial = naive_partial_match(choices, query, return_weights=True)[:5]
+        top_models = [
+            (model_name, pure_model(choices, query, model,
+                                    MAGIC_1 if model_name.startswith('magic1') else MAGIC_2, model_name,
+                                    return_weights=True)[:5])
+            for model_name, model in models.items()
+        ]
+        top_mixed = mixed_model(choices, query, last_model,
+                                MAGIC_1 if last_model_name.startswith('magic1') else MAGIC_2, last_model_name,
+                                return_weights=True)[:5]
+
+        # print(top_naive_partial)
+        # print(top_models)
+        # print(top_mixed)
+
+        headers = ['baseline'] + [m[0] for m in top_models] + ['mixed']
+        rows = []
+        for i in range(5):
+            if i < len(top_naive_partial):
+                naive_partial = f"{top_naive_partial[i][1]:>5.1%}: {top_naive_partial[i][0]}"
+            else:
+                naive_partial = "    ?: ?"
+            row = [naive_partial] \
+                  + [f"{m[1][i][1]:>5.1%}: {m[1][i][0]}" for m in top_models] \
+                  + [f"{top_mixed[i][1]:>5.1%}: {top_mixed[i][0]}"]
+            rows.append(row)
+
+        print(tabulate(rows, headers=headers))
+
+
 if __name__ == '__main__':
     models = {}
     last_model = None
@@ -152,17 +199,20 @@ if __name__ == '__main__':
     choices = load_choices()
     query_pairs = load_evaluation_queries()
 
-    t1, t2, t3, f, t, t10 = evaluate(naive_partial_match, query_pairs, choices, reverse_map=map_)
-    print(f"Naive Partial Match: t1={t1} t2={t2} t3={t3} t10={t10} f={f} t={t:.2f}")
-    t1, t2, t3, f, t, t10 = evaluate(naive_levenshtein_distance, query_pairs, choices, reverse_map=map_)
-    print(f"Naive Levenshtein: t1={t1} t2={t2} t3={t3} t10={t10} f={f} t={t:.2f}")
-    for model_name, model in models.items():
-        t1, t2, t3, f, t, t10 = evaluate(pure_model, query_pairs, choices, model=model, reverse_map=map_,
-                                    model_name=model_name,
-                                    magic_string=MAGIC_1 if model_name.startswith('magic1') else MAGIC_2)
-        print(f"{model_name} Pure: t1={t1} t2={t2} t3={t3} t10={t10} f={f} t={t:.2f}")
-    if last_model:
-        t1, t2, t3, f, t, t10 = evaluate(mixed_model, query_pairs, choices, model=last_model, reverse_map=map_,
-                                    model_name=last_model_name,
-                                    magic_string=MAGIC_1 if last_model_name.startswith('magic1') else MAGIC_2)
-        print(f"Mixed Model: t1={t1} t2={t2} t3={t3} t10={t10} f={f} t={t:.2f}")
+    if 'interactive' in sys.argv:
+        interactive_search(choices, models, map_, last_model, last_model_name)
+    else:
+        t1, t2, t3, f, t, t10 = evaluate(naive_partial_match, query_pairs, choices, reverse_map=map_)
+        print(f"Naive Partial Match: t1={t1} t2={t2} t3={t3} t10={t10} f={f} t={t:.2f}")
+        t1, t2, t3, f, t, t10 = evaluate(naive_levenshtein_distance, query_pairs, choices, reverse_map=map_)
+        print(f"Naive Levenshtein: t1={t1} t2={t2} t3={t3} t10={t10} f={f} t={t:.2f}")
+        for model_name, model in models.items():
+            t1, t2, t3, f, t, t10 = evaluate(pure_model, query_pairs, choices, model=model, reverse_map=map_,
+                                             model_name=model_name,
+                                             magic_string=MAGIC_1 if model_name.startswith('magic1') else MAGIC_2)
+            print(f"{model_name} Pure: t1={t1} t2={t2} t3={t3} t10={t10} f={f} t={t:.2f}")
+        if last_model:
+            t1, t2, t3, f, t, t10 = evaluate(mixed_model, query_pairs, choices, model=last_model, reverse_map=map_,
+                                             model_name=last_model_name,
+                                             magic_string=MAGIC_1 if last_model_name.startswith('magic1') else MAGIC_2)
+            print(f"Mixed Model: t1={t1} t2={t2} t3={t3} t10={t10} f={f} t={t:.2f}")
